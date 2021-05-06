60445ced8ad3f26765659f8261bb372ec18897c7
pavelklos <pavelkloscz@gmail.com>
2021-05-06 11:24:52 +0200

    Next Link & Router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,17 @@
 // _rfc
+import { useRouter } from "next/router";
 
 export default function EventPage() {
+  const router = useRouter();
+  console.log(router);
+
   return (
     <div>
-      <h1>Event Detail Page</h1>
+      <h1>Event Detail</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push("/")}>
+        Click (push : redirect to home page)
+      </button>
     </div>
   );
 }
