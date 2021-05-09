991de0945f918bb798f61d62717ea0715fd2f110
pavelklos <pavelkloscz@gmail.com>
2021-05-09 09:45:40 +0200

    getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,15 +1,18 @@
 // _rfc
 import { useRouter } from "next/router";
+import { API_URL } from "@/config/index";
 // import Layout from "../../components/Layout";
 import Layout from "@/components/Layout";
 
-export default function EventPage() {
+export default function EventPage(props) {
+  const { event } = props;
+  // console.log(event);
   const router = useRouter();
   // console.log(router);
 
   return (
     <Layout title={`DJ Event : ${router.query.slug}`}>
-      <h1>Event Detail</h1>
+      <h1>{event.name}</h1>
       <h3>{router.query.slug}</h3>
       <button onClick={() => router.push("/")} className='btn'>
         Click (push : redirect to home page)
@@ -17,3 +20,48 @@ export default function EventPage() {
     </Layout>
   );
 }
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const { slug } = query;
+  // console.log(query);
+
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const data = await res.json(); // events : array
+  // console.log(data);
+
+  return {
+    props: { event: data[0] },
+  };
+}
+
+// export async function getStaticProps(context) {
+//   const { params } = context;
+//   const { slug } = params;
+//   // console.log(params);
+
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const data = await res.json(); // events : array
+//   // console.log(data);
+
+//   return {
+//     props: { event: data[0] },
+//     revalidate: 60, // 60 sec : IF DATA WILL CHANGE -> NEW REQUEST (FETCH) TO GET NEW DATA
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const data = await res.json();
+//   // console.log(data);
+//   // { params: { slug: 'slug' } },
+//   const paths = data.map((event) => ({ params: { slug: event.slug } }));
+//   // console.log(paths);
+
+//   return {
+//     paths: paths,
+//     fallback: true,
+//     // false -> 404
+//     // true -> IF DATA WILL CHANGE -> NEW REQUEST (FETCH) TO GET NEW DATA
+//   };
+// }
