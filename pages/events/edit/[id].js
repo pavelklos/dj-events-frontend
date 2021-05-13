0b0a318b1061d5224becabf83dfd625e943e3aeb
pavelklos <pavelkloscz@gmail.com>
2021-05-13 18:59:05 +0200

    Edit Event & Image Preview

---
+++ pages/events/edit/[id].js
@@ -0,0 +1,182 @@
+// _rfc
+
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+import { FaImage } from "react-icons/fa";
+import { ToastContainer, toast } from "react-toastify";
+import moment from "moment";
+import "react-toastify/dist/ReactToastify.css";
+// import Layout from "../../components/Layout";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Form.module.css";
+
+export default function EditEventPage(props) {
+  const { event } = props;
+  const [values, setValues] = useState({
+    name: event.name,
+    performers: event.performers,
+    venue: event.venue,
+    address: event.address,
+    date: event.date,
+    time: event.time,
+    description: event.description,
+  });
+  // console.log(values.date);
+  const [imagePreview, setImagePreview] = useState(
+    event.image ? event.image.formats.thumbnail.url : null
+  );
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(values);
+
+    // Validation
+    const hasEmptyFields = Object.values(values).some(
+      (element) => element === ""
+    );
+    if (hasEmptyFields) {
+      // console.log("Please fill in all fields");
+      toast.error("Please fill in all fields");
+    }
+
+    // Strapi
+    const res = await fetch(`${API_URL}/events/${event.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    if (!res.ok) {
+      toast.error("Something Went Wrong");
+    } else {
+      const data = await res.json(); // data = event
+      router.push(`/events/${data.slug}`);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return (
+    <Layout title='Add New Event'>
+      <Link href='/event'>Go Back</Link>
+      <h1>Edit Event Page</h1>
+      <ToastContainer />
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor='name'>Event Name</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='performers'>Performers</label>
+            <input
+              type='text'
+              id='performers'
+              name='performers'
+              value={values.performers}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='venue'>Venue</label>
+            <input
+              type='text'
+              id='venue'
+              name='venue'
+              value={values.venue}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='address'>Address</label>
+            <input
+              type='text'
+              id='address'
+              name='address'
+              value={values.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='date'>Date</label>
+            <input
+              type='date'
+              id='date'
+              name='date'
+              // value={values.date}
+              value={moment(values.date).format("yyyy-MM-DD")}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='time'>Time</label>
+            <input
+              type='text'
+              id='time'
+              name='time'
+              value={values.time}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor='description'>Description</label>
+          <textarea
+            type='date'
+            id='description'
+            name='description'
+            value={values.description}
+            onChange={handleInputChange}
+          />
+        </div>
+        <input type='submit' value='Update Event' className='btn' />
+      </form>
+
+      <h2>Event Image</h2>
+      {imagePreview ? (
+        <Image src={imagePreview} width={170} height={100} objectFit='cover' />
+      ) : (
+        <div>
+          <p>No image uploaded</p>
+        </div>
+      )}
+      <div>
+        <button className='btn-secondary'>
+          <FaImage /> Set Image
+        </button>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { query, params } = context;
+  // const { id } = query;
+  const { id } = params;
+  // console.log(id);
+
+  const res = await fetch(`${API_URL}/events/${id}`);
+  const data = await res.json();
+  // console.log(data);
+
+  return {
+    props: {
+      event: data,
+    },
+  };
+}
