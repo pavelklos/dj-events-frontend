4b064aaed5fa73944e3c22e6a3eb635ba353b2bc
pavelklos <pavelkloscz@gmail.com>
2021-05-17 09:15:35 +0200

    Added : Gettting User For Dashboard + Updated UI

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -4,12 +4,17 @@ import { API_URL } from "@/config/index";
 import Layout from "@/components/Layout";
 
 export default function DashboardPage(props) {
-  const { events } = props;
-  console.log(events);
+  const { events, user } = props;
+  // console.log({ events, user });
+  const { username, email, created_at } = user;
+  const createdAt = new Date(created_at).toLocaleString("cs-CZ");
 
   return (
     <Layout>
-      <h1>Dashboard</h1>
+      <h1>Dashboard - Events ({events.length})</h1>
+      <p>
+        <b>{username}</b> (email: <b>{email}</b> - created: <b>{createdAt}</b>)
+      </p>
     </Layout>
   );
 }
@@ -18,17 +23,28 @@ export async function getServerSideProps(context) {
   const { token } = parseCookies(context.req);
   // console.log(token);
 
-  const res = await fetch(`${API_URL}/events/me`, {
+  // User's Events
+  const eventsRes = await fetch(`${API_URL}/events/me`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-  const data = await res.json();
+  const eventsData = await eventsRes.json();
+
+  // Logged in User
+  const userRes = await fetch(`${API_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const userData = await userRes.json();
 
   return {
     props: {
-      events: data,
+      events: eventsData,
+      user: userData,
     },
   };
 }
